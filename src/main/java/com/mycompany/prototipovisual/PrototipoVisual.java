ec426553ceda25be4a5d80974a8093cdcfaacaa7
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2023-08-23 00:32:18 -0500

    Primer commit

---
+++ src/main/java/com/mycompany/prototipovisual/PrototipoVisual.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prototipovisual;
+import com.mycompany.prototipovisual.Login
+/**
+ *
+ * @author gbval
+ */
+public class PrototipoVisual {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Login frLogin = new Login();
+        frLogin.setVisible(true);
+    }
+}
